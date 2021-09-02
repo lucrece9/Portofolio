2641a3f55d457c6a2fc89d62657ef545a838f443
lucrece9 <89991609+lucrece9@users.noreply.github.com>
2021-09-02 18:45:52 +0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['emailadress'];
+$message= $_POST['message'];
+$number= $_POST['mobile'];
+
+$to = "aliebonglucrece123@mail.com";
+
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message ."\r\n Mobile number =". $number;
+
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thank.html");
+?>
